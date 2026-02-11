3cc22083fb4add885331df40a90a51575753049c
David <dcljlong@gmail.com>
2026-02-11 22:07:50 +1300

    deploy: static site for GitHub Pages

---
+++ long-line-tracker/service-worker.js
@@ -0,0 +1,18 @@
+﻿self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('llt-v1').then(cache =>
+      cache.addAll([
+        'index.html',
+        'style.css',
+        'main.js',
+        'manifest.json'
+      ])
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
